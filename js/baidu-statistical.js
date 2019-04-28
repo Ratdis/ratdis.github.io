b7faf4d28089866a928c8dc7f73cccfc1adcbc0b
Ratdis <ratdis1@gmail.com>
2019-04-28 16:41:37 +0800

    Site updated: 2019-04-28 16:41:36

---
+++ js/baidu-statistical.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0b73533256002017937b0fe1e99c3b47";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
